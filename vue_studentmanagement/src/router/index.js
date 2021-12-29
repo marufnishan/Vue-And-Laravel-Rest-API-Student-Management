64e61fc2a688fe6b43d6c51a8e571260024720a2
marufnishan <nasim15-2328@diu.edu.bd>
2021-12-29 16:33:37 +0600

    Route Secure Added

--- vue_studentmanagement/src/router/index.js
+++ vue_studentmanagement/src/router/index.js
@@ -1,29 +1,66 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import store from '../store'
 import Login from '../views/auth/Login.vue'
 import Register from '../views/auth/Register.vue'
 import Home from '../views/pages/Home.vue'
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      guest: true
+    }
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: {
+      guest: true
+    }
   },
   {
     path: '/admin/dashboard',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      auth: true
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.auth)) {
+    if (!store.getters.GET_AUTH_STATUS) {
+      next({
+        name: 'Login'
+      })
+    } else {
+      next()
+    }
+  }
+
+
+  else if (to.matched.some(record => record.meta.guest)) {
+    if (store.getters.GET_AUTH_STATUS) {
+      next({
+        name: 'Home'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
 })
 
 export default router
